24afdebdb7e4b6dc48efb0302037b3f4dd9065aa
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-10 21:09:40 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,24 @@
 # code your #valid_move? method here
-
+# valid move neeeds to meet two conditions. the first condition is that the position_taken? == false AND the second condition is that the entry from the user is within the range of 1-9
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    return true
+  else
+    return false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+  elsif board[index] == "X"
+    return true
+  elsif board[index] == "O"
+    return true
+  end
+end
