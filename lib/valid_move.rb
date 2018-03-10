532c27fe8303c5d4ae9d98e74cacc9570b5571f4
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-10 21:04:49 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 # valid move neeeds to meet two conditions. the first condition is that the position_taken? == false AND the second condition is that the entry from the user is within the range of 1-9
 def valid_move?(board, index)
-  if position_taken? == false && (index-1).between?(0, 8)
+  if position_taken?(board, index) == false && (index-1).between?(0, 8)
     return true
   else
     return false
